d69ed5fcff6e6a614293b6fb49428906683e2828
ubergesundheit <ubergesundheit@googlemail.com>
2017-12-19 21:45:13 +0100

    update certificate generation

--- README.md
+++ README.md
@@ -6,15 +6,20 @@ In order to run it, you have to follow these steps:
 
 ### 1. Generate server and client certificates
 
-Read `generateServerAndClientCertificates.sh`. The file generates a self signed CA certificate file and two certificate and key files signed by the CA. You need the CA certificate and the server certificate and key on the server and the CA certificate and the client certificate and key on the client.
+- Install certstrap (`go get -u github.com/square/certstrap`)
+- Run `./genCerts.sh` to generate a self signed CA along with certificates for the mailer and a client. The certificates are used for client/server TLS.
 
 ### 2. Build it
 
 Preferred way to build the mailer is to build a Docker image from it. Just run `docker build -t sensebox/sensebox-mailer .` in this directory and you should have a working version of the mailer.
 
+You can also build it using `docker-compose build`
+
 ### 3. Run it
 
 The server can only be configured through environment variables. The easiest way is to use [docker-compose](https://github.com/docker/compose) to achieve this. Just use the supplied `docker-compose.yml`. All config keys are prefixed with `SENSEBOX_MAILER_`. Consider this when consulting the table below.
+
+Another option is to create a `docker-compose.override.yml` to override the values in the original `docker-compose.yml` with values of the generated `server_env.yml` and run it with `docker-compose up`
 
 You should configure the following variables:
 
---
+++ genCerts.sh
@@ -0,0 +1,49 @@
+
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+CA_NAME=openSenseMapCA
+SERVICE=mailer
+
+echo "Generate root CA"
+certstrap init --passphrase "" --expires "10 years" --common-name "${CA_NAME}"
+
+echo "Create certificate requests for the server and client"
+certstrap request-cert --passphrase "" --key "out/${CA_NAME}.key" --key-bits "4096" --common-name "${SERVICE}_server" --domain "${SERVICE},localhost"
+certstrap request-cert --passphrase "" --key "out/${CA_NAME}.key" --key-bits "4096" --common-name "${SERVICE}_client" --domain "${SERVICE},localhost"
+
+echo "Sign the certificate requests"
+certstrap sign --passphrase "" --expires "10 years" --CA "$CA_NAME" "${SERVICE}_server"
+certstrap sign --passphrase "" --expires "10 years" --CA "$CA_NAME" "${SERVICE}_client"
+
+# generate docker-compose.yml environment sections..
+cat << EOF > client_env.yml
+      OSEM_mailer_cert: |-
+$(cat out/${SERVICE}_client.crt | while read line; do
+  echo "        $line"
+done)
+      OSEM_mailer_key: |-
+$(cat out/${SERVICE}_client.key | while read line; do
+  echo "        $line"
+done)
+      OSEM_mailer_ca: |-
+$(cat out/${CA_NAME}.crt | while read line; do
+  echo "        $line"
+done)
+EOF
+
+cat << EOF > server_env.yml
+      SENSEBOX_MAILER_SERVER_CERT: |-
+$(cat out/${SERVICE}_server.crt | while read line; do
+  echo "        $line"
+done)
+      SENSEBOX_MAILER_SERVER_KEY: |-
+$(cat out/${SERVICE}_server.key | while read line; do
+  echo "        $line"
+done)
+      SENSEBOX_MAILER_CA_CERT: |-
+$(cat out/${CA_NAME}.crt | while read line; do
+  echo "        $line"
+done)
+EOF
